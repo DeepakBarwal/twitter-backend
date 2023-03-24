0a38bf3b7c4444ab93736bb863354720fb4ec4cc
DeepakBarwal <deepakbarwal98@gmail.com>
2023-03-24 19:15:34 +0530

    Added hashtag model and tweet model - v1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 const express = require('express');
 
 const connect = require('./config/database');
-const TweetRepository = require('./repositories/tweet-repository');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
     console.log('Server started on port ' + PORT);
     await connect();
-    
-    const repo = new TweetRepository();
-    const tweets = await repo.getAll(0,2);
-    console.log(tweets[0].contentWithEmail);
-    const tweet = await repo.create({content: 'yo'});
-    console.log(tweet);
+    console.log('MongoDB connected');
 });
