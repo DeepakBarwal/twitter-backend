a9020ecb5499f182e3bee02b3b83453f0016d870
DeepakBarwal <deepakbarwal98@gmail.com>
2023-03-28 11:20:28 +0530

    Updated comment model with refPath

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,12 +1,30 @@
 import mongoose from 'mongoose';
 
 const commentScema = new mongoose.Schema({
-    content:
-        {
-            type: String,
-            required: true
-        }
-});
+    content: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet', 'Comment']
+    },
+    commentable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel'
+    }
+},
+{
+    timestamps: true
+}
+);
 
 const Comment = mongoose.model('Comment', commentScema);
 
